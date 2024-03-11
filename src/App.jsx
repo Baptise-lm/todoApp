ffa3e197c30f0887c6b06a911763b78c01b4c01e
Baptise-lm <baptistelmng44@gmail.com>
2024-03-11 16:37:27 +0100

    Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import { Button } from '@nextui-org/react'
 import './App.css'
+import TodoList from './components/TodoList'
+
+const todos = [
+  {
+    title: 'TODO 1',
+    description: 'TODO DESCRIPTION 1'
+  },
+  {
+    title: 'TODO 2',
+    description: 'TODO DESCRIPTION 2'
+  },
+  {
+    title: 'TODO 3',
+    description: 'TODO DESCRIPTION 3'
+  },
+  {
+    title: 'TODO 4',
+    description: 'TODO DESCRIPTION 4'
+  }
+]
 
 function App () {
   return (
     <>
+      <TodoList
+        todos={todos}
+      />
       <Button
         variant='flat'
         color='primary'
