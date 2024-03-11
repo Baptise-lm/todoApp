e0ad6e88932f1145c92f27babbea408982819169
Baptise-lm <baptistelmng44@gmail.com>
2024-03-11 14:52:29 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from '@nextui-org/react'
+import './App.css'
+
+function App () {
+  return (
+    <>
+      <Button
+        variant='flat'
+        color='primary'
+      >
+        LA BIG BIG BITE DE ERWAN
+      </Button>
+    </>
+  )
+}
+
+export default App
